ac66f3b67138d83890120416a4db630e9af468b6
Kim Kira <kitabakim@gmail.com>
2026-03-19 22:40:21 +0200

    add bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+if not TOKEN:
+    raise ValueError("DISCORD_TOKEN not found in .env")
+
+bot.run(TOKEN)
